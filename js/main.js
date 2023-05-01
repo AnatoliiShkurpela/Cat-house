6ddb0a696cc1798a5de2184317fe2ea0f37dacfc
AnatoliiShkurpela <toha.aladin@gmail.com>
2023-05-01 19:48:10 +0300

    new

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
+const myName = "Andrii";
+const title = document.querySelector(".cats-title");
+const logotype = document.querySelector(".site-logo img");
+const body = document.querySelector("body");
+
+console.log(logotype);
+
+function onLogoClick() {
+  logotype.classList.toggle("is-hidden");
+}
+
+title.addEventListener("click", onLogoClick);
+
+const catPhoto = document.querySelector(".cat-photo");
+
+function onPhotoClick() {
+  catPhoto.src = "https://picsum.photos/200/300";
+  catPhoto.width = "296";
+  catPhoto.height = "370";
+}
+catPhoto.addEventListener("click", onPhotoClick);
 const slider = document.querySelector(".hero-list");
 const next = document.querySelector(".next");
 const prev = document.querySelector(".prev");
@@ -6,7 +27,7 @@ const mySiema = new Siema({
   selector: slider,
   loop: true,
   duration: 1000,
-  easing: "cubic-bezier(.17,.67,.32,1.34)",
+  easing: "cubic-bezier(.17,.67,.32,1.34)"
 });
 
 function onNextClick() {
@@ -21,7 +42,8 @@ next.addEventListener("click", onNextClick);
 prev.addEventListener("click", onPrevClick);
 
 const lightbox = new SimpleLightbox(".gallery a", {
-  overlayOpacity: 0.75,
+  closeText: "&#128514;",
+  fadeSpeed: 1000
 });
 
 AOS.init();
